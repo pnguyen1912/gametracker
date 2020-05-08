c9fe9d18bcc86fbaf13a2f794b55cb29b267a773
Patton Nguyen <41391357+pnguyen1912@users.noreply.github.com>
2020-05-08 16:53:43 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const video = document.getElementById('video');
+async function startCapture() {
+  let captureStream = null;
+  try {
+    captureStream = await navigator.mediaDevices.getDisplayMedia();
+  } catch (err) {
+    console.error("Error: " + err);
+  }
+  video.srcObject = captureStream
+  return captureStream;
+}
+startCapture()
+let timerId = null;
+const doOCR = async () => {
+  const c = document.createElement('canvas');
+  c.width = 40;
+  c.height = 10;
+  c.getContext('2d').drawImage(video, 1100, 1070, 40, 10, 0, 0, 40, 10);
+  let imgData = c.getContext('2d').getImageData(20, 5, 1, 1)
+  red = imgData.data[0];
+  green = imgData.data[1];
+  blue = imgData.data[2];
+  alpha = imgData.data[3];
+  let color = red + green + blue + alpha
+
+  if (color === 770) {
+    console.log('X ready')
+  } else {
+    console.log('Kill more')
+  }
+
+  document.body.append(c)
+};
+
+video.addEventListener('play', () => {
+  timerId = setInterval(doOCR, 1000);
+});
+video.addEventListener('pause', () => {
+  clearInterval(timerId);
+});
